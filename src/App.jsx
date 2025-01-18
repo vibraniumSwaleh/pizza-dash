c04415a6b995b9dcd7458efeef45bc8a37e2e804
vibraniumSwaleh <hus.swaleh@yahoo.com>
2025-01-18 00:06:15 +0000

    setup react router v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/menu',
+    element: <Menu />,
+  },
+]);
+
 function App() {
-  return <div>Hello Vite</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
