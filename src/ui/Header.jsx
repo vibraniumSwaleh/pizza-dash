98a53fb426b1542b24460ed3f45c0da1e64f962f
vibraniumSwaleh <hus.swaleh@yahoo.com>
2025-01-18 13:54:23 +0000

    setup AppLayout navigation

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <div>
+      <Link to='/'>Pizza Dash Co.</Link>
+      <p>User</p>
+    </div>
+  );
+}
+
+export default Header;
