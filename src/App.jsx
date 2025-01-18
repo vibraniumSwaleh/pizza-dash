354a674fcfb90ceb13d26670f3f5ec7e12fed314
vibraniumSwaleh <hus.swaleh@yahoo.com>
2025-01-18 18:42:25 +0000

    setup react-router form/action data POST

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './ui/Home';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Order, { loader as orderLoader } from './features/order/Order';
-import CreateOrder from './features/order/CreateOrder';
+import CreateOrder, {
+  action as createOrderAction,
+} from './features/order/CreateOrder';
 import Cart from './features/cart/Cart';
 import AppLayout from './ui/AppLayout';
 import Error from './ui/Error';
@@ -26,7 +28,12 @@ const router = createBrowserRouter([
         path: '/cart',
         element: <Cart />,
       },
-      { path: '/order/new', element: <CreateOrder /> },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+        action: createOrderAction,
+        errorElement: <Error />,
+      },
       {
         path: '/order/:orderId',
         element: <Order />,
