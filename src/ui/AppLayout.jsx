320976d5d4172e212bc82d1b6d9a99303461f2ce
vibraniumSwaleh <hus.swaleh@yahoo.com>
2025-01-24 23:16:55 +0000

    extend and overwrite tailwind config

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -2,7 +2,6 @@ import { Outlet, useNavigation } from 'react-router-dom';
 import Header from './Header';
 import CartOverview from '../features/cart/CartOverview';
 import Loader from '../utils/Loader';
-import SearchOrder from '../features/order/SearchOrder';
 
 function AppLayout() {
   const navigation = useNavigation();
