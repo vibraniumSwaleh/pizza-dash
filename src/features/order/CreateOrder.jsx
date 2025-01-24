24c1eee365475fca0527f4664323b43d421da75b
vibraniumSwaleh <hus.swaleh@yahoo.com>
2025-01-24 21:09:20 +0000

    make reusable button component

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
+import Button from '../../ui/Button';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -79,12 +79,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button
-            disabled={isSubmitting}
-            className="focus:ring-offest-2 inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:ring focus:ring-yellow-300 disabled:cursor-not-allowed"
-          >
+          <Button disabled={isSubmitting}>
             {isSubmitting ? 'Submitting...' : 'Order now'}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
---
+++ src/ui/LinkButton.jsx
@@ -0,0 +1,26 @@
+import { Link, useNavigate } from 'react-router-dom';
+
+function LinkButton({ children, to }) {
+  const navigate = useNavigate();
+
+  if (to === '-1')
+    return (
+      <button
+        className="text-sm text-blue-500 hover:text-blue-600 hover:underline"
+        onClick={() => navigate(+to)}
+      >
+        &larr; Go back
+      </button>
+    );
+
+  return (
+    <Link
+      className="text-sm text-blue-500 hover:text-blue-600 hover:underline"
+      to={to}
+    >
+      {children}
+    </Link>
+  );
+}
+
+export default LinkButton;
